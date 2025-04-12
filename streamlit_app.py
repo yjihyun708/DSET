54964f13d38715feec787a6685f5d509f71e927a
yjihyun708 <yjihyun708@gmail.com>
2025-04-12 14:53:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from PIL import Image
 # 모델 로드
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("model.h5")
+    model = tf.keras.models.load_model("keras_model.h5")
     return model
 
 model = load_model()
